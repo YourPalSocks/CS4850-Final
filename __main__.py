688dc6f82bfebb1fa0c3b0579472684980f1f52f
Ethan Heinlein <ethanMH2002@gmail.com>
2023-11-15 22:52:28 -0500

    Begin command implementation

--- __main__.py
+++ __main__.py
@@ -7,12 +7,12 @@ def main():
     root.title("CS4850 Final Project - Ethan Heinlein")
     # Configure window properties
     root.configure(background="snow")
-    root.geometry("800x500+50+50")
+    root.geometry("700x500+50+50")
     root.resizable(False, False)
     # Add component frames
     disp_frame = Frame(root) # Frame used for canvas and stats area
     disp_frame.pack(side=TOP)
-    state_num = Label(root, borderwidth=-2, text="1/n")
+    state_num = Label(root, borderwidth=2, text="1/n")
     state_num.pack(side=TOP)
     # Add components
     c = Canvas(disp_frame, bg="white", height=300, width=600)
@@ -20,11 +20,15 @@ def main():
     c.pack()
     stats = Label(disp_frame, borderwidth=2, text="Steps: 0\t\tTime: 0.0s")
     stats.pack()
-    # Text area
+    # Text area for information
     cmd_win = Cmd_Window(root)
-    cmd_win.configure(width=80)
-    cmd_win.pack(pady=10)
-    cmd_win.bind("<<Modified>>", on_modification)
+    cmd_win.configure(width=80, height=8)
+    cmd_win.pack()
+    # Cmd area
+    cmd_in = Text(root, width=82, height=1)
+    cmd_in.pack()
+    cmd_in.bind("<Return>",
+                lambda event, win=cmd_win, c_in=cmd_in:parse_command(win,c_in))
 
     root.mainloop()
 
@@ -36,8 +40,34 @@ def setup_canvas(canvas):
     b.draw_block(canvas)
     pass
 
-def on_modification(event):
-    print("HI")
+def parse_command(win, c_in):
+    # Get command from input
+    cmd = c_in.get('1.0', 'end-1c').replace('\n', '')
+    c_in.delete('1.0', END)
+    # Add it to window before parsing
+    log(win, f"{cmd}")
+    # Parse the command
+    if cmd == "clear": # Clear command window and last run
+        win.do_clear()
+    elif cmd == "next":
+        pass
+    elif cmd == "prev":
+        pass
+    elif cmd == "initial":
+        pass
+    elif cmd == "final":
+        pass
+    elif cmd == "run":
+        pass
+    else:
+        log(win, "ERROR: unknown command")
+
+# Easy function for writing stuff to the log
+def log(win, txt):
+    win.configure(state='normal')
+    win.insert(END, txt + "\n")
+    win.configure(state='disabled')
+
 
 if __name__ == "__main__":
     main()
--- cmd_window.py
+++ cmd_window.py
@@ -3,18 +3,12 @@ import tkinter as tk
 
 class Cmd_Window(scrolledtext.ScrolledText):
 
-    
     def __init__(self, parent):
         scrolledtext.ScrolledText.__init__(self)
         self.root = parent
+        self.config(state='disabled')
 
     def do_clear(self):
-        self.delete(0, tk.END)
-
-    # Due to python's nature, parse_command needs to be last in this class
-    cmd_table = {
-        "clear": do_clear
-    }
-
-    def parse_command(self, cmd):
-        pass
+        self.config(state='normal')
+        self.delete('1.0', tk.END)
+        self.config(state='disabled')
